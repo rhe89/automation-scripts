3f0647e2d4a76b5d8f4bf822e626c2dd064319e5
Roar Hoksnes Eriksen <roar@Mac-Mini-Kontor.local>
2025-08-28 06:09:29 +0200

    Settings exported

--- export-settings.sh
+++ export-settings.sh
@@ -37,8 +37,9 @@ echo "
 
 Commiting and pushing changes to origin"
 
-git add *
-git commit -m "Settings exported"
+git add * &&
+git add .* &&
+git commit -m "Settings exported" &&
 git push
 
 echo "
